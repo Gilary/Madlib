e8032e1281fc9e40afdaa8c797cc58d3dbe5c6ef
Gilary Brito <Gilary_FB@live.nl>
2018-03-24 18:10:52 +0100

    Change in structure and placing of items. functions, statics and classes added

--- Madlib/Program.cs
+++ Madlib/Program.cs
@@ -1,68 +1,89 @@
 ﻿using System;
 
 
-namespace Madlib
+namespace Game
 {
-    class Program
+    static class Madlib
     {
-        static void Main()
-        {
-            //declare variables
-            string Creature;
-            string Luminous;
-            string Ghastly;
-            string Spectral;
-            string Countryman;
-            string Farrier;
-            string Farmer;
-            string Dreadful;
-            string Apparition;
-            string Hound;
-            string Story;
+        //declare variables
+        static string Creature;
+        static string Luminous;
+        static string Ghastly;
+        static string Spectral;
+        static string Countryman;
+        static string Farrier;
+        static string Farmer;
+        static string Dreadful;
+        static string Apparition;
+        static string Hound;
+        static string Story;
 
+        public static void Run()
+        {
+            Start();
+            GetWords();
+            WriteStory();
+            End();
+        }
+
+        static void Start()
+        {
             //write out a header 
             Console.WriteLine("--------");
             Console.WriteLine("Welcome to Madlib");
             Console.WriteLine("--------");
-
+        }
+        static void GetWords()
+        {
             //ask player to enter words
-            Console.WriteLine("Please enter your favourite animal");
+            Console.Write("Please enter your favourite animal:");
             Creature = Console.ReadLine();
 
-            Console.WriteLine("please enter a adjective");
+            Console.Write("please enter a adjective:");
             Luminous = Console.ReadLine();
 
-            Console.WriteLine("please enter a adjective");
+            Console.Write("please enter a adjective:");
             Ghastly = Console.ReadLine();
 
-            Console.WriteLine("please enter a word you say the most");
+            Console.Write("please enter a word you say the most:");
             Spectral = Console.ReadLine();
 
-            Console.WriteLine("please enter a profession");
+            Console.Write("please enter a profession:");
             Countryman = Console.ReadLine();
 
-            Console.WriteLine("please enter a profession");
+            Console.Write("please enter a profession:");
             Farrier = Console.ReadLine();
 
-            Console.WriteLine("please enter a profession");
+            Console.Write("please enter a profession:");
             Farmer = Console.ReadLine();
 
             Console.Write("Please enter an adjective: ");
             Dreadful = Console.ReadLine();
 
-            Console.Write("Please enter a noun: ");
+            Console.Write("Please enter a noun:");
             Apparition = Console.ReadLine();
 
-            Console.Write("Please enter a noun: ");
+            Console.Write("Please enter a noun:");
             Hound = Console.ReadLine();
-
+        }
+        static void WriteStory()
+        {
             //write out finished story
             Story = "You agreed that it was a huge " + Creature + ", " + Luminous + ", " + Ghastly + ", and " + Spectral + ". I have cross-examined these men, one of them a hard-headed " + Countryman + ", one a " + Farrier + ", and one a moorland " + Farmer + ", who all tell the same story of this " + Dreadful + " " + Apparition + ", exactly corresponding to the " + Hound + " of the legend.";
             Console.WriteLine(Story);
-
+        }
+        static void End()
+        {
             //keep window open and prompt for exit
             Console.WriteLine("Please press enter to exit");
             Console.ReadKey();
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Madlib.Run();
         }
     }
 }
