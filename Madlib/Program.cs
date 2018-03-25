2a6253c8871491dd5dbb7f2121b8d33949e12bb5
Gilary Brito <Gilary_FB@live.nl>
2018-03-25 12:27:38 +0200

    Array and loop added

--- Madlib/Program.cs
+++ Madlib/Program.cs
@@ -1,23 +1,38 @@
 ﻿using System;
-
+using System.Globalization;
 
 namespace Game
 {
     static class Madlib
     {
         //declare variables
-        static string Creature;
-        static string Luminous;
-        static string Ghastly;
-        static string Spectral;
-        static string Countryman;
-        static string Farrier;
-        static string Farmer;
-        static string Dreadful;
-        static string Apparition;
-        static string Hound;
-        static string Story;
+        static string[] Words = {
+        "creature",
+        "luminous",
+        "ghastly",
+        "spectral",
+        "countryman",
+        "farrier",
+        "farmer",
+        "dreadful",
+        "apparition",
+        "hound"
+        };
 
+        static string[] Prompts = {
+        "creature",
+        "luminous",
+        "ghastly",
+        "spectral",
+        "countryman",
+        "farrier",
+        "farmer",
+        "dreadful",
+        "apparition",
+        "hound"
+        };
+        static string Story;
+        static string GameTitle;
         public static void Run()
         {
             Start();
@@ -36,41 +51,32 @@ namespace Game
         static void GetWords()
         {
             //ask player to enter words
-            Console.Write("Please enter your favourite animal:");
-            Creature = Console.ReadLine();
-
-            Console.Write("please enter a adjective:");
-            Luminous = Console.ReadLine();
-
-            Console.Write("please enter a adjective:");
-            Ghastly = Console.ReadLine();
-
-            Console.Write("please enter a word you say the most:");
-            Spectral = Console.ReadLine();
-
-            Console.Write("please enter a profession:");
-            Countryman = Console.ReadLine();
-
-            Console.Write("please enter a profession:");
-            Farrier = Console.ReadLine();
-
-            Console.Write("please enter a profession:");
-            Farmer = Console.ReadLine();
-
-            Console.Write("Please enter an adjective: ");
-            Dreadful = Console.ReadLine();
-
-            Console.Write("Please enter a noun:");
-            Apparition = Console.ReadLine();
-
-            Console.Write("Please enter a noun:");
-            Hound = Console.ReadLine();
+            for (int i = 0; i < Words.Length; i++)
+            {
+                Console.Write("Please enter a/an " + Prompts[i] + ": ");
+                Words[i] = Console.ReadLine();
+            }
+           
+            //clear the console window
+            Console.Clear();
         }
         static void WriteStory()
         {
+            //Concatenate strings to make a title
+            GameTitle = "The " + Words[1] + " " + Words[2] + " " + Words[0];
+
+            //So we can capitalize the words in our title
+            TextInfo TitleCase = new CultureInfo("en-US", false).TextInfo;
+            GameTitle = TitleCase.ToTitleCase(GameTitle);
+
+            Console.WriteLine(GameTitle);
+
+            //change the title of the application window
+            Console.Title = GameTitle;
+
             //write out finished story
-            Story = "You agreed that it was a huge " + Creature + ", " + Luminous + ", " + Ghastly + ", and " + Spectral + ". I have cross-examined these men, one of them a hard-headed " + Countryman + ", one a " + Farrier + ", and one a moorland " + Farmer + ", who all tell the same story of this " + Dreadful + " " + Apparition + ", exactly corresponding to the " + Hound + " of the legend.";
-            Console.WriteLine(Story);
+            Story = "They all agreed that it was a huge {0}, {1}, {2}, and {3}.\nI have cross-examined these men, one of them a hard-headed {4},\none a {5}, and one a moorland {6}, who all tell the same story\nof this {7} {8}, exactly corresponding to the {9} of the legend.";
+            Console.WriteLine(Story, Words[0], Words[1], Words[2], Words[3], Words[4], Words[5], Words[6], Words[7], Words[8], Words[9]);
         }
         static void End()
         {
